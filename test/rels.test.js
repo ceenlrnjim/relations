dbe460f5ea41ea95ee8e1bf37952865d05285ae6
ceenlrnjim <kirkwojl@hotmail.com>
2013-03-01 09:06:50 -0500

    tests for distinct and related functions

--- test/rels.test.js
+++ test/rels.test.js
@@ -64,3 +64,47 @@ exports.testMap = function(test) {
     test.done();
 
 };
+
+exports.testDistinct = function(test) {
+    var rels = require("../src/rels.js");
+    var and = require("../src/utils.js").and;
+    var propEq = rels.propEq;
+    var data = new rels.Relation([{a:1, b:'a'},
+                                  {a:2, b:'b'},
+                                  {a:2, b:'c'},
+                                  {a:2, b:'b'},
+                                  {a:1, b:'a'}]);
+    
+    var result = data.distinct();
+    test.ok(result.length() === 3);
+    test.ok(result.select(rels.propEq("a",1)).length() == 1);
+    test.ok(result.select(and(propEq("a",2), propEq("b","b"))).length() === 1);
+    test.ok(result.select(and(propEq("a",2), propEq("b","c"))).length() === 1);
+    test.done();
+
+}
+
+exports.testDivide = function(test) {
+    // straight outta wikipedia
+    var rels = require("../src/rels.js");
+    var completed = new rels.Relation([ {s: "Fred", t: "db1"},
+                                        {s: "Fred", t: "db2"},
+                                        {s: "Fred", t: "comp1"},
+                                        {s: "Eugene", t: "db1"},
+                                        {s: "Eugene", t: "compiler1"},
+                                        {s: "Sarah", t: "db1"},
+                                        {s: "Sarah", t: "db2"}]);
+    var dbProject = new rels.Relation([{ t:"db1"}, {t:"db2"}]);
+    var result = completed.divideBy(dbProject).data;
+    test.ok(result.length === 2, "length was " + result.length);
+    test.ok(result[0].s === "Fred");
+    test.ok(result[1].s === "Sarah");
+    test.done();
+};
+exports.testAnd = function(test) {
+    var and = require("../src/utils.js").and;
+    var rangetest = and(function(a) { return a > 5; }, function(a) { return a < 10; });
+    test.ok(rangetest(7));
+    test.ok(!rangetest(1));
+    test.done();
+};
