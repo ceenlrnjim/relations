8c4cc28da4472f2936216bc45369f6c6978abcb9
ceenlrnjim <kirkwojl@hotmail.com>
2013-03-01 08:03:16 -0500

    tests for map and appendDerived

--- test/rels.test.js
+++ test/rels.test.js
@@ -31,3 +31,36 @@ exports.testSelect = function(test) {
    test.ok(id3.b === 'c');
    test.done();
 };
+
+exports.testDerive = function(test) {
+    var rels = require("../src/rels.js");
+    var data = new rels.Relation([{a:1, b:'a'},
+                                  {a:2, b:'b'},
+                                  {a:3, b:'c'},
+                                  {a:4, b:'d'},
+                                  {a:5, b:'e'}]);
+    var updated = data.appendDerived(function(row) { return { asq: row.a * row.a }; });
+    test.ok(updated.select(rels.propEq("a", 1)).data[0].asq === 1);
+    test.ok(updated.select(rels.propEq("a", 2)).data[0].asq === 4);
+    test.ok(updated.select(rels.propEq("a", 3)).data[0].asq === 9);
+    test.ok(updated.select(rels.propEq("a", 4)).data[0].asq === 16);
+    test.ok(updated.select(rels.propEq("a", 5)).data[0].asq === 25);
+    test.done();
+};
+
+exports.testMap = function(test) {
+    var rels = require("../src/rels.js");
+    var data = new rels.Relation([{a:1, b:'a'},
+                                  {a:2, b:'b'},
+                                  {a:3, b:'c'},
+                                  {a:4, b:'d'},
+                                  {a:5, b:'e'}]);
+    var updated = data.map(function(row) { return { a: row.a * row.a, b: row.b }; });
+    test.ok(updated.select(rels.propEq("a", 1)).data[0].b === 'a');
+    test.ok(updated.select(rels.propEq("a", 4)).data[0].b === 'b');
+    test.ok(updated.select(rels.propEq("a", 9)).data[0].b === 'c');
+    test.ok(updated.select(rels.propEq("a", 16)).data[0].b === 'd');
+    test.ok(updated.select(rels.propEq("a", 25)).data[0].b === 'e');
+    test.done();
+
+};
