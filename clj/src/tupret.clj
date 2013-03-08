1433a45c13832acd72a8e4dda3439e0d80292b99
ceenlrnjim <kirkwojl@hotmail.com>
2013-03-08 15:18:58 -0500

    reimplemented binding-set
    use when and reduce rather than manual recursion

--- clj/src/tupret.clj
+++ clj/src/tupret.clj
@@ -14,21 +14,14 @@
         (vector? tuple-collection) (range 0 (count tuple-collection))
         :else nil)) ; TODO: probably need a throws
 
-; TODO add support for list/vector tuples as well - no keys, just sequnce of matching values
 (defn binding-set
-  "patter and tuple are both maps"
   [pattern tuple]
-  (loop [result {}
-         keyseq (key-seq pattern)]
-    (if (empty? keyseq) result
-      (let [k (first keyseq)
-            pval (get pattern k)
-            tval (get tuple k)]
-        (if (fresh? pval) 
-            (recur (assoc result (fresh-name pval) tval) (next keyseq))
-            (if (= pval tval) 
-                (recur result (next keyseq))
-                (recur nil nil)))))))
+  ; divide the keys in pattern into those that are fresh (fk) and those that specify values (vk)
+  (let [{vk false fk true} (group-by #(fresh? (get pattern %)) (key-seq pattern))]
+    ; when all the keys whose values are not fresh have the same values in the pattern and the tuple
+    (when (every? #(= (get pattern %) (get tuple %)) vk)
+      ; assoc all the keys whose values are fresh in the pattern to the equivalent value in this tuple
+      (reduce #(assoc %1 (fresh-name (get pattern %2)) (get tuple %2)) {} fk))))
 
 
 (defn pattern-matches
