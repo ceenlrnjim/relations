2cb8a6faa21afea1fc8c631b88e392e3f3d302e1
ceenlrnjim <kirkwojl@hotmail.com>
2013-03-25 13:36:28 -0300

    added test for commutativity of hash joins

--- clj/test/rels/test.clj
+++ clj/test/rels/test.clj
@@ -22,5 +22,33 @@
       (is (= (:z sample) 1))
       (is (= (:ea (:e sample)) "abc")))))
 
+(def s [ {:score 1 :prodid :a :reqtid :1 }
+         {:score 1 :prodid :a :reqtid :2 }
+         {:score 1 :prodid :a :reqtid :3 }
+         {:score 1 :prodid :a :reqtid :4 }
+         {:score 2 :prodid :b :reqtid :1 }
+         {:score 2 :prodid :b :reqtid :2 }
+         {:score 2 :prodid :b :reqtid :3 }
+         {:score 2 :prodid :b :reqtid :4 }
+         {:score 3 :prodid :c :reqtid :1 }
+         {:score 3 :prodid :c :reqtid :2 }
+         {:score 3 :prodid :c :reqtid :3 }
+         {:score 3 :prodid :c :reqtid :4 }
+         {:score 4 :prodid :d :reqtid :1 }
+         {:score 4 :prodid :d :reqtid :2 }
+         {:score 4 :prodid :d :reqtid :3 }
+         {:score 4 :prodid :d :reqtid :4 }])
+
+(def p [ {:prodid :a :proddesc "Product A" }
+         {:prodid :b :proddesc "Product B" }
+         {:prodid :c :proddesc "Product B" }
+         {:prodid :d :proddesc "Product D" } ])
+
+(deftest test-commutative-hash
+  (println "Starting commutative test")
+  (let [sp (hash-join s p [:prodid :prodid =])
+        ps (hash-join p s [:prodid :prodid =])]
+    (is (= (count sp) (count ps)))))
+
 (run-tests 'rels.test)
   
